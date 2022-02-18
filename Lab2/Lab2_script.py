c496ffd45f047896486348f77566712c11174751
JacobLin6868 <71741541+JacobLin6868@users.noreply.github.com>
2022-02-18 03:57:18 -0500

    final

--- Lab2/Lab2_script.py
+++ Lab2/Lab2_script.py
@@ -1,8 +1,10 @@
 import math
 
+import data as data
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import random
 import pandas as pd
 from numpy import logical_or
 
@@ -10,8 +12,7 @@ data = pd.read_csv('data/patients.txt', delimiter=";", header=None)
 list_of_columns = data.columns = ['patientNo', 'gender', 'vist', 'HR', 'SBP', 'DBP', 'DX', 'AE']
 print('\n')
 print('Raw Data')
-print(data)
-print('\n')
+
 
 # replace all empty cells with NAN
 # data.replace(r'^\s*$', np.nan, regex=True, inplace=True)
@@ -26,27 +27,38 @@ data['DX'].replace(r'^\s*$', np.nan, regex=True, inplace=True)
 data['DX'].fillna(999, inplace=True)
 # print(data)
 # print('\n')
+
+
 # DBP
-data['DBP'].replace(r'^\s*$', 0, regex=True, inplace=True)
+data['DBP'].replace(r'^\s*$', 60, regex=True, inplace=True)
+data['DX'].replace(r'[a-zA-Z]', 60, regex=True, inplace=True)
 data['DBP'] = data['DBP'].astype(str).astype(int)
-a = np.array(data['DBP'].values.tolist())
-data['DBP'] = np.where(logical_or(a < 60, a > 120), 60, a).tolist()
+data['DBP'] = data['DBP'].apply(lambda x: [80 if x < 60 else (120 if x > 120 else x)])
+data['DBP'] = data['DBP'].astype(str)
+data['DBP'] = data['DBP'].replace(r'\D', '', regex=True)
+
+
 # print(data)
 
 
 # SBP
 data['SBP'].replace(r'^\s*$', 0, regex=True, inplace=True)
 data['SBP'] = data['SBP'].astype(str).astype(int)
-a = np.array(data['SBP'].values.tolist())
-data['SBP'] = np.where(logical_or(a < 80, a > 200), 80, a).tolist()
+data['SBP'] = data['SBP'].apply(lambda x: [80 if x < 80 else (200 if x > 200 else x)])
+data['SBP'] = data['SBP'].astype(str)
+data['SBP'] = data['SBP'].replace(r'\D', '', regex=True)
 
 # HR
 data['HR'].replace(r'^\s*$', 0, regex=True, inplace=True)
 data['HR'].replace(r'[a-zA-Z]', 0, regex=True, inplace=True)
 data['HR'] = data['HR'].astype(str).astype(int)
-a = np.array(data['HR'].values.tolist())
-data['HR'] = np.where(logical_or(a < 40, a > 100), 40, a).tolist()
-# print(data)
+data['HR'] = data['HR'].apply(lambda x: [80 if x < 40 else (100 if x > 100 else x)])
+data['HR'] = data['HR'].astype(str)
+data['HR'] = data['HR'].replace(r'\D', '', regex=True)
+
+print(data)
+
+
 # vist
 data['vist'].astype(str)
 data['vist'].replace(r'^\s*$', '01011900', regex=True, inplace=True)
@@ -55,8 +67,7 @@ data['vist'].replace(r'^\s*$', '01011900', regex=True, inplace=True)
 data['vist'].replace(r'[a-zA-Z]+', np.nan, regex=True, inplace=True)
 data['vist'].fillna('01011900', inplace=True)
 # print(data)
-
-# new dataframe for data analysis
+# new dataframe for breaking down MM DD YYYY
 date_data = pd.DataFrame(columns=['reference', 'MM', 'DD', 'YYYY'])
 date_data.astype(str)
 date_data['reference'] = data['vist']
@@ -73,22 +84,30 @@ date_data['DD'] = date_data['DD'].fillna(31)
 # print(date_data)
 date_data['YYYY'] = date_data['YYYY'][date_data['YYYY'].between(0, 1999)]
 date_data['YYYY'] = date_data['YYYY'].fillna(1999)
-date_data['YYYY'] = date_data['YYYY'].apply(lambda x: x + 1900 if x < 100 else ( x + 1000 if x < 1000 else x))
+date_data['YYYY'] = date_data['YYYY'].apply(lambda x: x + 1900 if x < 100 else (x + 1000 if x < 1000 else x))
 date_data = date_data.astype(int).astype(str)
 date_data['MM'] = date_data['MM'].apply(lambda x: '{0:0>2}'.format(x))
 date_data['DD'] = date_data['DD'].apply(lambda x: '{0:0>2}'.format(x))
 # print(date_data)
 
 data['vist'] = date_data['MM'] + date_data['DD'] + date_data['YYYY']
+# print(data)
+
+# patientNo
+data['patientNo'].replace(r'[a-zA-Z]', 199, regex=True, inplace=True)
+data['patientNo'].replace(r'^\s*$', 199, regex=True, inplace=True)
+data['patientNo'] = data['patientNo'].astype(str).astype(int)
+max_value = data['patientNo'].max()
+
+
+data['patientNo'] = data['patientNo'].where(~data['patientNo'].duplicated(), random.randint(200, 200))
+data['patientNo'] = data['patientNo'].where(~data['patientNo'].duplicated(), random.randint(201, 201))
+data['patientNo'] = data['patientNo'].where(~data['patientNo'].duplicated(), random.randint(202, 202))
+data['patientNo'] = data['patientNo'].where(~data['patientNo'].duplicated(), random.randint(203, 203))
+data['patientNo'] = data['patientNo'].astype(int).astype(str)
+data['patientNo'] = data['patientNo'].apply(lambda x: '{0:0>3}'.format(x))
+print('\n')
+print('Cleaned Data')
 print(data)
 
-# print(date_data)
-#
-# # patientNo
-# # data['patientNo'].replace(r'^\s*$', 0, regex=True, inplace=True)
-# # data['patientNo'].replace(r'\D', 0, regex=True, inplace=True)
-# #
-# # data['patientNo'].astype(str).astype(int)
-# # print(data)
-# # print('\n')
-# # print(data.dtypes)
+
